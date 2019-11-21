58343984c4bd9b6b98938c6dbb0d95e89676d5d8
Lasse Steinnes <lasse.steinnes93@gmail.com>
2019-11-21 12:25:06 +0100

    Created skeleton code

---
+++ main.py
@@ -0,0 +1,4 @@
+"""
+Main script to fit model, storing table of information
+(toi) (and possibly some figures).
+"""
---
+++ methods.py
@@ -0,0 +1,25 @@
+class Regression():
+    """
+    A class for different regression methods
+    """
+
+    def __init__(self,):
+
+        
+    def svm(self,):
+        """
+        Regression using Support Vector Machines (SVM)
+        """
+        return
+
+    def linear(self,):
+        """
+        Linear regression method
+        """
+        return
+
+    def weak_regressor(self,):
+        """
+        Weak regressor method
+        """
+        return
